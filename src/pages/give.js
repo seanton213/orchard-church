1b643bc1ed0e195436e7c822e6d7a90877e166e1
seanton213 <seanton213@gmail.com>
2025-04-07 20:51:05 -0700

    fix build

--- src/pages/give.js
+++ src/pages/give.js
@@ -2,13 +2,11 @@ import * as React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import * as styles from "../components/index.module.css"
-import { useNavigate } from "react-router-dom";
 
 const Give = () => {
-  const navigate = useNavigate();
   
   const handleClick = () => { 
-    navigate("https://www.paypal.com/donate/?hosted_button_id=PEFXXU3ZNEQD6")
+    window.open("https://www.paypal.com/donate/?hosted_button_id=PEFXXU3ZNEQD6")
   }
   
   return (
